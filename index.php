cccc092e62e20d43e1e710a7da7cf4a4b0f38dc8
Isaac Lim <isaaclimdc@gmail.com>
2013-11-08 11:53:40 -0500

    ...

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Title</title>
+  <meta charset="UTF-8">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script type="text/javascript" src="scripts/nlp.js"></script>
+</head>
+
+<body>
+  <form id="inputForm">
+    Emotion: <input type="text" name="inputEmotion"><br>
+    Object: <input type="text" name="inputObject"><br>
+  </form>
+  <div onClick="findAllTweets()">
+    <h2>Go!</h2>
+  </div>
+  <div id="dispTweets">
+  </div>
+</body>
+
+</html>
---
+++ push.sh
@@ -0,0 +1,5 @@
+mv index.html index.php
+git add *
+git commit -a --m "..."
+git push heroku master
+mv index.php index.html
